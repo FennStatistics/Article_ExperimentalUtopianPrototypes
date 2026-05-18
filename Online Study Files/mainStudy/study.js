5650e593f4043f898a1dc9f9a7badb655187dc15
Julius <fenn.statistics@posteo.de>
2026-05-18 20:33:49 +0200

    refactor: update testing flags in globals.js and comment out debug plugin in study.js

--- Online Study Files/mainStudy/study.js
+++ Online Study Files/mainStudy/study.js
@@ -35,7 +35,7 @@ const study = new lab.flow.Sequence({
   plugins: [
     new lab.plugins.Metadata(),
     // new lab.plugins.Fullscreen(),
-    new lab.plugins.Debug(), // comment out finally !!!
+    // new lab.plugins.Debug(), // comment out finally !!!
     // new lab.plugins.Download()
   ],
   content: [
